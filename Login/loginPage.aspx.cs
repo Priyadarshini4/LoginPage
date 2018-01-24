8aad38985d3e0dea2c7d526c49138c87d4dad4c8
Priya <Priyadarshini.professional@gmail.com>
2018-01-25 00:11:12 +0530

    First Commit

---
+++ Login/loginPage.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Login
+{
+    public partial class loginPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=./;Initial Catalog=CodeSolution;Persist Security Info=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from login where username='" + txtusername.Text + "' and pwd ='" + txtpassword.Text + "'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                Response.Redirect("Details.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('Please enter valid Username and Password')</script>");
+            }
+        }
+
+    }
+}
